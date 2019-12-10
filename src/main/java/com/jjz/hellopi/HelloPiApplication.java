c8a8047fbbd95fb10c406ae3f18bf096420853ea
JJ Zabkar <jjzabkar@gmail.com>
2019-12-09 16:31:22 -0800

    initial commit

---
+++ src/main/java/com/jjz/hellopi/HelloPiApplication.java
@@ -0,0 +1,13 @@
+package com.jjz.hellopi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloPiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jjz/hellopi/HelloPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjz.hellopi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
